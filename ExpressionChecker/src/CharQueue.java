a262a850841e7e3f67aa81e1673a56250a71131b
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 08:39:56 +0530

    Ready to submit

---
+++ ExpressionChecker/src/CharQueue.java
@@ -0,0 +1,62 @@
+public class CharQueue {
+    private Node firstNode;
+    private Node lastNode;
+    private int size;
+
+    public CharQueue() {
+        size = 0;
+    }
+
+    public Character dequeue() {
+        if (size == 0) {
+            return null;
+        }
+        char temp = firstNode.data;
+        if (size == 1) {
+            firstNode = null;
+        } else {
+            firstNode.next.prev = null;
+            firstNode = firstNode.next;
+        }
+
+        size -- ;
+        return temp;
+    }
+
+    public void enqueue(char character) {
+        if (size == 0) {
+            firstNode = new Node(character);
+            lastNode = firstNode;
+        } else if (size == 1) {
+            lastNode = new Node(character, firstNode);
+            firstNode.next = lastNode;
+        } else {
+            lastNode = new Node(character, lastNode);
+            lastNode.prev.next = lastNode;
+        }
+
+        size ++ ;
+    }
+
+    public void clear() {
+        size = 0;
+        firstNode = null;
+        lastNode = null;
+    }
+
+    private static class Node {
+        char data;
+        Node next;
+        Node prev;
+
+        public Node(char data) {
+            this.data = data;
+        }
+
+        public Node(char data, Node prev) {
+            this.data = data;
+            this.prev = prev;
+            prev.next = this;
+        }
+    }
+}
