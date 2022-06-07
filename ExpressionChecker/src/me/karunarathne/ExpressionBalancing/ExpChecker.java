17618060123cd1879a878965cc54cfa96f41da8a
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 07:26:41 +0530

    we

--- ExpressionChecker/src/me/karunarathne/ExpressionBalancing/ExpChecker.java
+++ ExpressionChecker/src/me/karunarathne/ExpressionBalancing/ExpChecker.java
@@ -2,8 +2,19 @@ package me.karunarathne.ExpressionBalancing;
 
 public class ExpChecker {
     private static CharQueue queue;
+    private static String expression;
+    private static int expLength;
 
     static {
         queue = new CharQueue();
+    }
+
+    public static boolean checkBalance (String exp) {
+        expression = exp;
+        expLength = exp.length();
+    }
+
+    private static boolean isBalanced () {
+
     }
 }
