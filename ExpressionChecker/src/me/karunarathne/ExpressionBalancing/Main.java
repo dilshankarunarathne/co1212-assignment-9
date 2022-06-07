85c3155ff7362fc62b79ad203c3111c2d0136c01
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 07:55:31 +0530

    Update Main.java

--- ExpressionChecker/src/me/karunarathne/ExpressionBalancing/Main.java
+++ ExpressionChecker/src/me/karunarathne/ExpressionBalancing/Main.java
@@ -1,7 +1,11 @@
 package me.karunarathne.ExpressionBalancing;
 
 public class Main {
-    private static String [] expressions = {"x{R-(5/2)}"};
+    private static String [] expressions = {
+            "x{R-(5/2)}",
+            "[p+1-(3xr+5)]",
+            
+    };
 
     public static void main(String[] args) {
 
