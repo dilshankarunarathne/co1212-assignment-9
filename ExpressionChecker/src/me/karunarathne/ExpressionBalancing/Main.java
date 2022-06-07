03e985d4ac524d3983c4d672839354a84385bb01
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 08:01:10 +0530

    Update Main.java

--- ExpressionChecker/src/me/karunarathne/ExpressionBalancing/Main.java
+++ ExpressionChecker/src/me/karunarathne/ExpressionBalancing/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         Arrays.stream(expressions).forEach(
-                expression -> checkExp(expression)
+                Main::checkExp
         );
     }
 
