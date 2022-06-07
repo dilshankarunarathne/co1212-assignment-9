0e62c76fbc0f1de89e71ef8ea7546d8b4d5d6e0e
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-06-07 07:57:57 +0530

    Update Main.java

--- ExpressionChecker/src/me/karunarathne/ExpressionBalancing/Main.java
+++ ExpressionChecker/src/me/karunarathne/ExpressionBalancing/Main.java
@@ -1,15 +1,21 @@
 package me.karunarathne.ExpressionBalancing;
 
+import java.util.Arrays;
+
 public class Main {
     private static String [] expressions = {
             "x{R-(5/2)}",
             "[p+1-(3xr+5)]",
             "{2t(5r{m-1)",
             "[{3x-4(2r-1)+8}+97]",
-            
+
     };
 
     public static void main(String[] args) {
-
+        Arrays.stream(expressions).forEach(
+                expression -> checkExp(expression)
+        );
     }
+
+    
 }
